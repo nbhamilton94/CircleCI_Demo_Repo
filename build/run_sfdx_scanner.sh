217b92e80d579166cee61e1d336921779069859b
Norman Hamilton <normanhamilton@RyansiPone11Pro.localdomain>
2024-03-08 01:13:26 -0600

    build fix

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -3,12 +3,12 @@ sudo apt update
 sudo apt-get install openjdk-11-jdk
 echo "Install SFDX Scanner"
 #echo -e 'y/n' | 
-sudo npm i -g @salesforce/sfdx-scanner/
+#sudo npm i -g @salesforce/sfdx-scanner/
 #echo "i am here:"
 #pwd
 #cd /node_modules/\@salesforce/sfdx-scanner/
 #sf plugins link
-# echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
+echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
 
 echo "Running SFDX Scanner"
 #running the scanner on the default folder in the repository (force-app/main/default) place the results in sfdxScannerAnalysis.csv
