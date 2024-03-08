ae0f10b194723e1b4667d355df707a64acad6a23
Norman Hamilton <normanhamilton@RyansiPone11Pro.localdomain>
2024-03-08 01:21:44 -0600

    build fix

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -3,13 +3,13 @@ sudo apt update
 sudo apt-get install openjdk-11-jdk
 echo "Install SFDX Scanner"
 #echo -e 'y/n' | 
-#sudo npm i -g @salesforce/sfdx-scanner/
+sudo npm i -g @salesforce/sfdx-scanner
 #echo "i am here:"
 #pwd
 #cd /node_modules/\@salesforce/sfdx-scanner/
 #sf plugins link
 #echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
-sf plugins install @salesforce/sfdx-scanner
+#sf plugins install @salesforce/sfdx-scanner
 echo "Running SFDX Scanner"
 #running the scanner on the default folder in the repository (force-app/main/default) place the results in sfdxScannerAnalysis.csv
 #declare that violations cause an error (--violations-cause-error), if you don't do this it won't stop your circleci from deploying to production
